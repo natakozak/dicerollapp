7f9162f11402d1021c2c2d2a4d43b904a7200373
natakozak <kozaknata02@gmail.com>
2023-05-18 19:16:47 +0300

    fifth commit

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
+        /*rollButton.setOnClickListener {
             //val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
             //toast.show()
             //Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
@@ -20,14 +20,36 @@ class MainActivity : AppCompatActivity() {
             rollButton.setOnClickListener {
                 rollDice()
             }
-        }
+        }**/
+
+        // Set a click listener on the button to roll the dice when the user taps the button
+        rollButton.setOnClickListener { rollDice() }
+
+        // Do a dice roll when the app starts
+        rollDice()
+
     }
 
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
+
         val diceImage: ImageView = findViewById(R.id.imageView)
-        diceImage.setImageResource(R.drawable.dice_2)
+
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 
     class Dice(private val numSides: Int) {
